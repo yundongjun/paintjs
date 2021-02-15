1c1cd84801a1849a6e3876a7ff8f02bf8d04e3d8
yundongjun <68623554+yundongjun@users.noreply.github.com>
2021-02-15 10:41:03 +0900

    2-15-3.md

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const canvas = document.getElementById("jsCanvers");
+const canvas = document.getElementById("jsCanvas");
 
-function onMouseMove(event){
-    console.log(event);
+let painting = false;
+
+function stopPainting(){
+    painting =false;
 }
 
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x,y);
+}
+
+function onMouseDown(event){
+    painting=true;
+}
+
+function onMouseUp(event){
+    stopPainting();
+}
+
+
 if(canvas){
-    canvas.addEventListener("mousemove",on, onMouseMove);
+    canvas.addEventListener("mousemove",onMouseMove);
+    canvas.addEventListener("mousedown",onMouseDown);
+    canvas.addEventListener("mouseup",onMouseUp);
+    canvas.addEventListener("mouseleave",stopPainting);
 }
